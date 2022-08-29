4d0225182171dbcc48287140cfb2e1835031fcfb
Jumipatel <jumibenpatel18@gnu.ac.in>
2022-08-29 08:58:20 +0530

    xml

--- app/src/main/java/com/example/mad_practical3_20012011083/LoginActivity.kt
+++ app/src/main/java/com/example/mad_practical3_20012011083/LoginActivity.kt
@@ -3,17 +3,18 @@ package com.example.mad_practical3_20012011083
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
+import com.example.mad_practical3_20012011083.databinding.ActivityLoginBinding
 import com.example.mad_practical3_20012011083.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+// Constratint layout
         setSupportActionBar(binding.toolbar)
     }
 }
--- app/src/main/java/com/example/mad_practical3_20012011083/RegistrationActivity.kt
+++ app/src/main/java/com/example/mad_practical3_20012011083/RegistrationActivity.kt
@@ -4,16 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
 import com.example.mad_practical3_20012011083.databinding.ActivityMainBinding
+import com.example.mad_practical3_20012011083.databinding.ActivityRegistrationBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class RegistrationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistrationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+// Linear layout
         setSupportActionBar(binding.toolbar)
     }
 }
